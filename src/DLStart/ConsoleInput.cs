5574eb3ff9aba22f9385320032b2d8bde5cb49c4
林亚 <Alis.Lin@outlook.com>
2023-06-03 22:09:21 +0800

    调整输入方法

---
+++ src/DLStart/ConsoleInput.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLStart
+{
+    internal class ConsoleHelper
+    {
+        /// <summary>
+        /// 读取控制台输入
+        /// </summary>
+        /// <param name="infos">提示信息</param>
+        /// <param name="filter">检查输入值是合法检测</param>
+        /// <returns></returns>
+        public static string? Read(string[] infos, int? emptyLine = 0, Func<string?, bool>? filter = null)
+        {
+            Print(infos, emptyLine);
+            var line = Console.ReadLine();
+            while (!(filter?.Invoke(line) ?? true))
+            {
+                Print(infos, emptyLine);
+                line = Console.ReadLine();
+            }
+
+            return line;
+        }
+
+        /// <summary>
+        /// 打印
+        /// </summary>
+        /// <param name="infos">提示信息</param>
+        /// <param name="emptyLine">打印前插入的空行数（默认为：0）</param>
+        public static void Print(string[] infos, int? emptyLine = 0)
+        {
+            var count = emptyLine ?? 0;
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine("");
+            }
+            foreach (var item in infos)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int GetInt(string s, int defaultValue = 0)
+        {
+            if (int.TryParse(s, out var k))
+            {
+                return k;
+            }
+            return defaultValue;
+        }
+    }
+}
--- src/DLStart/Program.cs
+++ src/DLStart/Program.cs
@@ -1,6 +1,8 @@
 ﻿using DLStart;
 using System.Diagnostics;
+using System.Formats.Tar;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 var targetName = "";
 var configFile = "config.json";
@@ -26,65 +28,123 @@ if (!File.Exists(app))
     return;
 }
 
+ConsoleHelper.Print(new string[]
+                        {
+                            "1. 输入需要保存的路径(默认使用配置路径)",
+                            "2. 输入需要保存的名称，该名称将作为文件夹名称",
+                            "3. 按照提示输入m3u8链接地址（如果使用task.txt作为输入，直接回车）",
+                            "-----------------------------",
+                        }, 2
+                    );
 
-Console.WriteLine("");
-Console.WriteLine("");
-Console.WriteLine("1. 输入需要保存的路径(默认使用配置路径)");
-Console.WriteLine("2. 输入需要保存的名称，该名称将作为文件夹名称");
-Console.WriteLine("3. 按照提示输入m3u8链接地址（如果使用task.txt作为输入，直接回车）");
-
-Console.WriteLine("-----------------------------");
-Console.WriteLine("");
-Console.WriteLine($"输入需要保存的路径(默认使用配置路径 [{config?.SavePath}]):");
-// 等待输入，将输入的字串作为保存路径，如果为空使用默认路径
-string savePath = Console.ReadLine();
+//// 等待输入，将输入的字串作为保存路径，如果为空使用默认路径
+string savePath = ConsoleHelper.Read(new string[] { $"输入需要保存的路径(默认使用配置路径 [{config?.SavePath}]):" }, 1)?.Trim() ?? "";
 if (string.IsNullOrEmpty(savePath))
 {
     savePath = config?.SavePath ?? "Downloads";
 }
 
-while (string.IsNullOrEmpty(targetName))
-{
-    Console.WriteLine("");
-    Console.WriteLine("输入需要保存的名称，该名称将作为文件夹名称:");
-    // 等待输入，将输入的字串作为targetName,如果没有则提示再次输入
-    targetName = Console.ReadLine();
-}
+var taskFile_select_Flag = false;
+var taskFile_name = "";
+
+targetName = ConsoleHelper.Read(new string[] { "输入需要保存的名称，该名称将作为文件夹名称（直接回车选择任务导入）:" }, 1,
+    x =>
+    {
+        if (string.IsNullOrWhiteSpace(x))
+        {
+            // 如果targetName为空，使用 task 文件作为输入。选择task文件
+            // 获取task文件列表
+            var di = new DirectoryInfo(".");
+            var taskFile_list = new List<FileInfo>(di.GetFiles("task_*.task"));
+            var taskFile_tips = new List<string>();
+            taskFile_tips.Add("选择任务文件（直接回车返回名称输入）：");
+            taskFile_tips.AddRange(taskFile_list.Select(f => $"{taskFile_list.IndexOf(f) + 1}) {f.Name}"));
+            var m = ConsoleHelper.Read(taskFile_tips.ToArray(), 1);
+            if (string.IsNullOrWhiteSpace(m))
+            {
+                return false;
+            }
+            var task_index = ConsoleHelper.GetInt(m);
+            if (task_index > 0 && task_index <= taskFile_list.Count)
+            {
+                taskFile_select_Flag = true;
+                taskFile_name = taskFile_list[task_index - 1].FullName;
+                return true;
+            }
+            return false;
+        }
+        return true;
+
+    });
+
+var path = "";
 
 var session = "";
-while (string.IsNullOrEmpty(session))
+if (taskFile_select_Flag && !string.IsNullOrWhiteSpace(taskFile_name))
 {
-    Console.WriteLine("");
-    Console.WriteLine("输入第几季（直接回车跳过）:");
-    // 等待输入，将输入的字串作为targetName,如果没有则提示再次输入
-    var s = Console.ReadLine();
-    if (int.TryParse(s, out var k))
+    var lines = new List<string>();
+    // 使用task文件加载
+    lines.AddRange(File.ReadAllLines(taskFile_name));
+    ConsoleHelper.Print(new string[]
     {
-        if (k > 0)
+        $"使用 [{taskFile_name}] 文件输入任务：",
+    }, 1);
+    ConsoleHelper.Print(lines.ToArray());
+
+    targetName = lines[0].Trim();
+    session = lines[1].Trim();
+
+
+    lines.RemoveAt(0);
+    lines.RemoveAt(0);
+
+    path = Path.Combine(savePath, targetName);
+    var task_sub = new DLTask(app, config?.TempPath, config?.AfterTaskRun, config?.AfterTaskRunSync);
+    var task_index = 1;
+    foreach (var item in lines)
+    {
+        var link_match = Regex.Match(item, @"(\d+),(.*)");
+        task_index = ConsoleHelper.GetInt(link_match.Groups[1].Value);
+        var item_link = link_match.Groups[2].Value.Trim();
+        var targetFile = $"{targetName}.{session}e{task_index.ToString("00")}";
+        targetFile = $"{targetName}.{session}e{task_index.ToString("00")}";
+        if (!string.IsNullOrWhiteSpace(item_link))
         {
-            session = $"s{k.ToString("00")}";
+            //var arg = $"{item.Trim()} --save-name {targetFile} --save-dir {path}";
+            task_sub.Add(item_link, targetFile, path);
         }
+        task_index++;
     }
+
+    task_sub.Run();
+    task_sub.Wait();
+    Console.WriteLine("任务执行完成");
+
+    return;
 }
 
-var path = Path.Combine(savePath, targetName);
+
+session = ConsoleHelper.Read(new string[] { "输入第几季（直接回车跳过）:" }, 1) ?? "1";
+session = ConsoleHelper.GetInt(session, 1).ToString("00");
+
+path = Path.Combine(savePath, targetName);
 
 var index = 1;
+var startIndex = index;
 // when input is not empty string,set index add 1 and input string to filename,continue wait input
 var link = "123";
 var task = new DLTask(app, config?.TempPath, config?.AfterTaskRun, config?.AfterTaskRunSync);
-var taskHistory = $"task_{DateTime.Now.ToString("yyyyMMddHHmmss")}_{DateTime.Now.Microsecond}.txt";
+var taskHistory = $"task_{DateTime.Now.ToString("yyyyMMddHHmmss")}_{targetName}_{DateTime.Now.Microsecond}.task";
 var taskFile_Flag = true;
 while (!string.IsNullOrEmpty(link))
 {
     var targetFile = $"{targetName}.{session}e{index.ToString("00")}";
-    Console.WriteLine("");
-    if (taskFile_Flag)
-    {
-        Console.WriteLine("如果使用task.txt作为输入，直接回车");
-    }
-    Console.WriteLine($"输入选集 m3u8 链接，当前第 {index} 集（直接回车结束添加，输入数字改变集数）：");
-    link = Console.ReadLine()?.Trim() ?? "";
+
+    link = ConsoleHelper.Read(new string[] {
+        taskFile_Flag?"如果使用task.txt作为输入，直接回车":"",
+        $"输入选集 m3u8 链接，当前第 {index} 集（直接回车结束添加，输入数字改变集数）：",
+    }, 1)?.Trim() ?? "";
+
     // check link is number type
     if (int.TryParse(link, out var k))
     {
@@ -125,7 +185,12 @@ while (!string.IsNullOrEmpty(link))
     }
     //var para = $"{link} --save-name {targetFile} --save-dir {path}";
     task.Add(link, targetFile, path);
-    File.AppendAllText(taskHistory, $"{link}\r\n");
+    if (taskFile_Flag)
+    {
+        File.AppendAllText(taskHistory, $"{targetName}\r\n");
+        File.AppendAllText(taskHistory, $"{session}\r\n");
+    }
+    File.AppendAllText(taskHistory, $"{index},{link}\r\n");
     index++;
     taskFile_Flag = false;
 }
